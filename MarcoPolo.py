c80a355005f86f3f2bd3f6c9f37a1fc69fccd937
Marc-Antoine <marc22fortier@hotmail.com>
2015-03-09 11:15:42 +0100

    Important refactor of every class. We can now explore devices using LLDP protocol.

--- MarcoPolo.py
+++ MarcoPolo.py
@@ -18,197 +18,392 @@ import paramiko
 
 DEFAULT_MAX_BYTES = 2048*2048
 DEFAULT_TIMEOUT = 10
+DEFAULT_USERNAME = "admin"
+DEFAULT_PASSWORD = ""
 
 config = ConfigParser.RawConfigParser()
 
-class CDPNetworkDevice(object):
+
+class NetworkDevice(object):
 	def __init__(
 			self,
-			port=None,
-			device_id=None,
-			address_type=None,
-			address=None,
-			platform=None,
-			capability=None,
-			device_port=None,
-			version=None):
+			mac_address=None,
+			ip_address=None,
+			ip_address_type=None,
+			system_name=None,
+			system_description=None,
+			local_port=None,
+			remote_port=None,
+			supported_capabilities=None,
+			enabled_capabilities=None):
 		
-		self.port = port
-		self.device_id = device_id
-		self.address_type = address_type
-		self.address = address
-		self.platform = platform
-		self.capability = capability
-		self.device_port = device_port
-		self.version = version
+		self.mac_address = mac_address
+		self.ip_address = ip_address
+		self.ip_address_type = ip_address_type
+		self.system_name = system_name
+		self.system_description = system_description
+		self.local_port = local_port
+		self.remote_port = remote_port
+		self.supported_capabilities = supported_capabilities
+		self.enabled_capabilities = enabled_capabilities
+		
+	def attributeCDPInformation(self, key, value):
+		if "DeviceID" in key:
+			self.mac_address = value
+		elif "DevicePort" in key:
+			self.local_port = value
+		elif "Port" in key:
+			self.remote_port = value
+		elif "AddressType" in key:
+			self.ip_address_type = value
+		elif "Address" in key:
+			self.ip_address = value
+		elif "Platform" in key:
+			self.system_description = value
+		elif "Capability" in key:
+			self.enabled_capabilities = value
+		elif "Version" in key:
+			pass # This attribute is the same as 'Platform'
+		else:
+			print("Unexpected key '{0}'.".format(key))
+			
+	def attributeLLDPInformationToDevice(self, key, value):
+		if "LocalPort" in key:
+			self.remote_port = value
+		elif "ChassisType" in key:
+			pass # We don't need this information
+		elif "ChassisId" in key:
+			self.mac_address = value
+		elif "PortType" in key:
+			pass # We don't need this information
+		elif "PortId" in key:
+			self.local_port = value
+		elif "SysName" in key:
+			self.system_name = value
+		elif "SystemDescr" in key:
+			self.system_description = value
+		elif "PortDescr" in key:
+			pass # Do we need this information?
+		elif "Pvid" in key:
+			pass # Often empty information
+		elif "SystemCapabilitiesSupported" in key:
+			self.supported_capabilities = value
+		elif "SystemCapabilitiesEnabled" in key:
+			self.enabled_capabilities = value
+		elif "Type" in key:
+			self.ip_address_type = value
+		elif "Address" in key:
+			self.ip_address = value
+		elif "EndpointClass" in key:
+			pass # Useless information
+		else:
+			print("Unexpected key '{0}'.".format(key))
+		
+	def is_valid_cdp_device(self):
+		#print("'{0}' == 'Switch': {1}".format(self.enabled_capabilities, self.enabled_capabilities == "Switch"))
+		#print("'{0}' != 'Unsupportedformat': {1}".format(self.ip_address, self.ip_address != "Unsupportedformat"))
+		return \
+		(
+			self.enabled_capabilities == "Switch" and \
+			self.ip_address != "Unsupportedformat"
+		)
+		
+	def is_valid_lldp_device(self):
+		return \
+		(
+			"Switch" in self.system_description and \
+			self.ip_address is not None
+		)
+		
+	def __str__(self):
+		return \
+		( 
+			"\
+			MAC 			: {0}\n \
+			IP Address 		: {1}\n \
+			IP Address type 	: {2}\n \
+			Name 			: {3}\n \
+			Description 		: {4}\n \
+			Local port 		: {5}\n \
+			Remote port 		: {6}\n \
+			Supported capabilities 	: {7}\n \
+			Enabled capabilities 	: {8}\n".format(
+		    self.mac_address,
+			self.ip_address,
+			self.ip_address_type,
+			self.system_name,
+			self.system_description,
+			self.local_port,
+			self.remote_port,
+			self.supported_capabilities,
+			self.enabled_capabilities)
+		)
+
+
+class NetworkDeviceBuilder(object):
+	def __init__():
+		pass
+	
+	@staticmethod
+	def buildNetwordDeviceFromLLDPDetail(lldp_result):
+		"""Creates NetworkDevice objects from the LLDP remote device \
+		information detail of a Hewlett-Packard\xc2 switch"""
+		devices = []
+		currentDevice = NetworkDevice()
+		
+		try:
+			# Parsing "show lldp info remote-device [port]" command's result
+			for rawline in lldp_result.splitlines():	
+				line = rawline.rstrip()
+				
+				if ':' in line:
+					key, value = NetworkDeviceBuilder\
+						.extractKeyAndValueFromLine(line)
+					#print("Attributing key '{0}' and value '{1}'"
+						#.format(key, value))
+					currentDevice.attributeLLDPInformationToDevice(key, value)
+					
+				elif '#' in line:
+					devices.append(currentDevice)
+					currentDevice = NetworkDevice()
+					
+		except Exception:
+			print("Could not build network devices from '{0}'."
+				.format(lldp_result))
+				
+		return devices
 
 	@staticmethod
-	def buildNetwordDevicesFrom(cdp_result):
-		"""Creates CDPNetworkDevice objects from the CDP neighbors detail \
+	def buildNetwordDevicesFromCDPInformation(cdp_result):
+		"""Creates NetworkDevice objects from the CDP neighbors detail \
 		information of a Hewlett-Packard\xc2 switch"""
 		devices = []
-		currentDevice = CDPNetworkDevice()
+		currentDevice = NetworkDevice()
 		
-		if cdp_result is None:
-			print("Could not build network devices from '{0}'."
-				.format(output_file))
-				
-		# Parsing cdp neighbors information/detail
-		for rawline in cdp_result.splitlines():	
-			try:
+		try:
+			# Parsing "show cdp neighbors detail" command's result
+			for rawline in cdp_result.splitlines():	
 				line = rawline.rstrip()
 				if ':' in line:
-					tokens = line.split()
-					key = [' '.join(tokens[:tokens.index(':')])][0]
-					value = [' '.join(tokens[tokens.index(':')+1:])][0]
-
-					CDPNetworkDevice.attributeToDevice(currentDevice, key, value)
-			
+					key, value = NetworkDeviceBuilder\
+						.extractKeyAndValueFromLine(line)
+					
+					currentDevice.attributeCDPInformation(key, value)
 				elif '-----' in line:
 					devices.append(currentDevice)
-					currentDevice = CDPNetworkDevice()
-			except Exception:
-				print("There was a problem parsing the cdp neighbors information.")
-				pass
+					currentDevice = NetworkDevice()
+		
+		except Exception as e:
+			print("Could not build network devices: '{0}'."
+				.format(e))
 	
 		# Not forgetting the last device since it's not followed by a dotted line ('-----')	
 		devices.append(currentDevice)
 		
 		switches = 0
 		for device in devices:
-			if CDPNetworkDevice.is_valid_device(device):
+			if device.is_valid_cdp_device():
 				switches += 1
 		
 		print("Found {0} valid switche(s) out of {1} neighbor(s)."
 			.format(switches, len(devices)))
 		
 		return devices
-		
-	@staticmethod
-	def attributeToDevice(device, key, value):
-		if "Device ID" in key:
-			device.device_id = value
-		elif "Device Port" in key:
-			device.device_port = value
-		elif "Port" in key:
-			device.port = value
-		elif "Address Type" in key:
-			device.address_type = value
-		elif "Address" in key:
-			device.address = value
-		elif "Platform" in key:
-			device.platform = value
-		elif "Capability" in key:
-			device.capability = value
-		elif "Version" in key:
-			device.version = value
-		else:
-			print("Unexpected key '{0}'.".format(key))
 	
-	@staticmethod	
-	def is_valid_device(device):
-		valid = \
-		(
-			device.capability == "Switch" and \
-			device.address != "Unsupported format"
-		)
-		return valid
+	@staticmethod
+	def extractLocalPortsFromLLDPInformation(lldp_result):
+		local_ports = []
+		try:	
+			# Parsing ports from "show lldp info remote-device" command's result
+			for rawline in lldp_result.splitlines():	
+				line = rawline.rstrip()
+				if '|' in line:
+					tokens = line.split()
+					port = tokens[:tokens.index('|')][-1]
+					
+					if port != 'LocalPort' and \
+						NetworkDeviceBuilder.extractSystemNameFromLine(rawline) \
+						is not None:
+						local_ports.append(port)		
+		except Exception as e:
+			print("Could not extract local ports from '{0}'."
+				.format(lldp_result))
+				
+		return local_ports
+	
+	@staticmethod
+	def extractSystemNameFromLine(line):
+		system_name = None
+		try:
+			target = line[57:].replace(" ", "")
+			if target != "" and "..." not in target:
+				system_name = target
+		except Exception as e:
+			print("Could not extract system name from '{0}'."
+				.format(line))
+		
+		return system_name
+
+	@staticmethod
+	def extractKeyAndValueFromLine(line):
+		tokens = line.split(':')
+		key = tokens[0].replace(" ", "")
+		value = tokens[1].replace(" ", "")
+		return (key, value)
 
 
 class NetworkExplorer(object):
 	def __init__(self):
-		pass
+		self.ssh = paramiko.SSHClient()
+		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		
+		self.shell = None
+		
+		self.ssh_timeout = DEFAULT_TIMEOUT
+		self.ssh_max_bytes = DEFAULT_MAX_BYTES
+		self.ssh_username = DEFAULT_USERNAME
+		self.ssh_password = DEFAULT_PASSWORD
+		
+		try:
+			self.ssh_timeout = config.getfloat('SSH', 'Timeout')
+			self.ssh_max_bytes = config.getint('SSH', 'MaximumBytesToReceive')
+			self.ssh_username = config.get('SSH', 'Username')
+			self.ssh_password = config.get('SSH', 'Password')
+			
+		except ConfigParser.Error as cpe:
+			print("Configuration error: {0}".format(cpe))
+		except Exception as e:
+			print("Unexpected error: {0}".format(e))
+
+	def explore_cdp(self, origin, devices):
+		"""Recursively explores all neighbors of a Hewlett-Packard\xc2 switch 
+		using the CDP protocol."""
+		self._open_ssh_connection(origin.ip_address)
+		
+		neighbors = self._get_cdp_neighbors()
+		
+		self._close_ssh_connection()
+		
+		for neighbor in neighbors:
+			if neighbor.is_valid_cdp_device() and neighbor.mac_address not in devices:
+				devices[neighbor.mac_address] = neighbor
+						
+				print("\n{0} [{1}] ------> [{2}] {3}\n"
+					.format(origin.ip_address, neighbor.remote_port,
+							neighbor.local_port, neighbor.ip_address))
+				
+				#Let's recursively explore my neighbors' neighbors
+				devices = self.explore_cdp(neighbor, devices)
+		
+		return devices
 	
-	def explore(self, origin, devices):
-		"""Recursively explores all cdp neighbors switches of a \
-		Hewlett-Packard\xc2 switch."""
-		command = 'show cdp neighbors detail\n'
-		raw_result = self._send_ssh_command(origin.address, command)
+	def explore_lldp(self, origin, devices):
+		"""Recursively explores all neighbors of a Hewlett-Packard\xc2 switch 
+		using the LLDP protocol."""
+		self._open_ssh_connection(origin.ip_address)
 		
-		if raw_result is not None:
-			neighbors = CDPNetworkDevice.buildNetwordDevicesFrom(raw_result)
+		open_ports = self._get_lldp_open_ports(origin)
 		
-			for neighbor in neighbors:	
-				if CDPNetworkDevice.is_valid_device(neighbor) and \
-					neighbor.device_id not in devices:
+		neighbors = self._get_lldp_neighbors(origin, open_ports)
+		
+		self._close_ssh_connection()
+		
+		for neighbor in neighbors:
+			if neighbor.is_valid_lldp_device() and neighbor.mac_address not in devices:
+				devices[neighbor.mac_address] = neighbor
 					
-					devices[neighbor.device_id] = neighbor
-							
-					print("\n{0} [{1}] ------> [{2}] {3}\n"
-					.format(origin.address, origin.port,
-							neighbor.port, neighbor.address))
-					
-					#Recursive fun starts here!
-					devices = self.explore(neighbor, devices)
+				print("\n{0} [{1}] ------> [{2}] {3}\n"
+					.format(origin.ip_address, neighbor.remote_port,
+							neighbor.local_port, neighbor.ip_address))
+			
+				#Let's recursively explore my neighbors' neighbors
+				devices = self.explore_lldp(neighbor, devices)
 		
 		return devices
 		
-	def _send_ssh_command(self, hostname, command):
+	def _get_lldp_open_ports(self, origin):
+		command = "show lldp info remote-device\n"
+		remote_devices_list = self._send_ssh_command(command, True, False)
+		
+		return NetworkDeviceBuilder\
+				.extractLocalPortsFromLLDPInformation(remote_devices_list)
+		
+	def _get_lldp_neighbors(self, origin, open_ports):
+		devices_details = ""
+		
+		for port in open_ports:
+			command = "show lldp info remote-device {0}\n".format(port)
+			result =  self._send_ssh_command(command, False, False)
+			if result is not None:
+				devices_details += result
+		
+		return NetworkDeviceBuilder\
+				.buildNetwordDeviceFromLLDPDetail(devices_details)
+	
+	def _get_cdp_neighbors(self):
+		command = 'show cdp neighbors detail\n'
+		devices_details = self._send_ssh_command(command, True, True)
+		
+		return NetworkDeviceBuilder\
+				.buildNetwordDevicesFromCDPInformation(devices_details)
+		
+	def _open_ssh_connection(self, hostname):
+		print("Connecting to {0}...".format(hostname))
+		
+		try:
+			self.ssh.connect(hostname,
+				username=self.ssh_username,
+				password=self.ssh_password)
+				
+			self.shell = self.ssh.invoke_shell()
+			self.shell.settimeout(self.ssh_timeout)
+		except socket.error as se:
+			print("Socket error: {0}".format(se))
+		except paramiko.AuthenticationException as pae:
+			print("Authentication error: {0}".format(pae))
+		except Exception as e:
+			print("Unexpected error: {0}".format(e))
+		
+		print("Connected to {0}.".format(hostname))
+		
+	def _close_ssh_connection(self):
+		try:
+			self.shell.close()
+			self.ssh.close()
+			print("Successfully closed ssh connection.")
+		except Exception:
+			print("Could not close ssh connection.")
+		
+	def _send_ssh_command(self, command, prepare, close):
 		"""Opens a SSH connection using paramiko with a Hewlett-Packard\xc2 \
 		switch in order to retrieve the output data."""
 		
-		ssh = paramiko.SSHClient()
-		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		
-		ssh_timeout = DEFAULT_TIMEOUT
-		ssh_max_bytes = DEFAULT_MAX_BYTES
-		
+		result = None	
 		try:
-			print("Connecting to {0}...".format(hostname))
-			
-			ssh_timeout = config.getfloat('SSH', 'Timeout')
-			ssh_max_bytes = config.getint('SSH', 'MaximumBytesToReceive')
-			
-			ssh_username = config.get('SSH', 'Username')
-			ssh_password = config.get('SSH', 'Password')
-			
-			ssh.connect(hostname, username=ssh_username, password=ssh_password)
+			# These 'time.sleep()' are used to give enough time for the switch to operate
+			if prepare:
+				print("Preparing command...")
+				time.sleep(1)
+				self.shell.send('\n') # Bypassing the HP switch welcome page
+				time.sleep(2)
+				self.shell.send('no page\n') # Asking the switch to display all data at once
+				time.sleep(1)
 				
+				self.shell.recv(self.ssh_max_bytes) # Flushing useless data
+			
+			print("Executing command '{0}'...".format(command.rstrip()))
+			self.shell.send(command)
+			
+			# TODO Change for a while loop until we've got everything
+			time.sleep(5) # Waiting for the server to display all the data
+			
+			result = self.shell.recv(self.ssh_max_bytes)
+			
 		except socket.error as se:
 			print("Socket error: {0}".format(se))
-			return None
-		except paramiko.AuthenticationException as pae:
-			print("Authentication error: {0}".format(pae))
-			return None
-		except ConfigParser.Error as cpe:
-			print("Configuration error: {0}".format(cpe))
-			return None
 		except Exception as e:
 			print("Unexpected error: {0}".format(e))
-			return None
-				
-		print("Connected to {0}.".format(hostname))
-		
-		shell = ssh.invoke_shell()
-		shell.settimeout(ssh_timeout)
-
-		# These 'time.sleep()' are used to give enough time for the switch to operate
-		
-		try:
-			print("Preparing command...")
-			time.sleep(1)
-			shell.send('\n') # Bypassing the HP switch welcome page
-			time.sleep(2)
-			shell.send('no page\n') # Asking the switch to display all data at once
-			time.sleep(1)
-		
-			shell.recv(ssh_max_bytes) # Flushing useless data
-		
-			print("Executing command...")
-		
-			shell.send(command)
-			time.sleep(5) # Waiting for the server to display all the data
-			# TODO Change for a while loop until we've got everything		
-			result = shell.recv(ssh_max_bytes)
-
-			shell.close()
-			ssh.close()
-			print("Closed connection to {0}.".format(hostname))
-			
-		except socket.timeout as st:
-			print("Socket error: {0}".format(st))
-			return None
-		
+					
 		return result
 
 def _parseargs():
@@ -229,15 +424,19 @@ if __name__ == "__main__":
 			config.read(config_file)
 			
 			source_address = config.get('DEFAULT', 'SourceAddress')	
-			origin = CDPNetworkDevice(address = source_address, port = '?')
+			origin = NetworkDevice(ip_address = source_address, local_port = '?')
 
 			MarcoPolo = NetworkExplorer()
 			devices = {}
 		
 			print("\nStarting Marco Polo's network exploration.\n")
-			MarcoPolo.explore(origin, devices)
+			MarcoPolo.explore_cdp(origin, devices)
+			#MarcoPolo.explore_lldp(origin, devices)
 	
 			print("\nMarco Polo found {0} devices.".format(len(devices)))
+			
+			for device in devices.values():
+				print(str(device))
 			
 		except ConfigParser.Error as cpe:
 			print("Configuration error. {0}".format(cpe))
